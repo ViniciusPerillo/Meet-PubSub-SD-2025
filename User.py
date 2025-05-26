58c90cbcfebc938db7cb7fe811a91d8919a15f06
Vinicius Perillo <vinicius.perillo25@gmail.com>
2025-05-26 11:21:04 -0300

    fix: encoding ips

--- User.py
+++ User.py
@@ -77,30 +77,32 @@ class User:
         router.bind(f'tcp://[::]:{ROUTER_PORT}')
 
         while self.on_room:
-            ip, username, bytes_password = router.recv_multipart()
-            length = bytes_password>>8
-            password = (bytes_password - (bytes_password>>8<<8)).to_bytes(length, 'big').decode('utf-8')
+            bytes_ip, bytes_username, bytes_password = router.recv_multipart()
+            ip = bytes_ip.decode('utf-8')
+            password = bytes_password.decode('utf-8')
 
             if password == self.password:
-                router.send_multipart[ip, b'', convert_ipv6_list_to_bin(self.peers_addr)]
+                router.send_multipart[ip, b'', str(self.peers_addr)[1:-1].encode('utf-8')]
                 with self.lock:
-                    self.peers_addr.append(convert_bin_to_ipv6_str(ip))
+                    self.peers_addr.append(ip)
 
-            self.publisher.send_multipart([b'status', username.encode('utf-8'), (ip<<1 | 1)])
+            self.publisher.send_multipart([b'status', bytes_username, (ip + '1').encode('utf-8')])
         
         router.close(1)
 
     def _getHost(self, ip: str, password: str):
         dealer: zmq.Socket = self.context.socket(zmq.DEALER)
         dealer.setsockopt(zmq.IPV6, 1)
-        dealer.setsockopt(zmq.IDENTITY, convert_ipv6_str_to_bin(self.ipv6))
         dealer.setsockopt(zmq.LINGER, 0)  
         dealer.setsockopt(zmq.RCVTIMEO, 3000)
         dealer.connect(f'tcp://[{ip}]:{ROUTER_PORT}') 
-        bytes_password = len(password)<<8 | int.from_bytes(password.encode('utf-8'), 'big')
+        
+        bytes_ip = self.ipv6.encode('utf-8')
+        bytes_username = self.username.encode('utf-8')
+        bytes_password = password.encode('utf-8')
 
         try:
-            dealer.send_multipart([convert_ipv6_str_to_bin(self.ipv6), self.username, bytes_password])
+            dealer.send_multipart([bytes_ip, bytes_username, bytes_password])
             _, _, list_ips = dealer.recv_multipart()
         except zmq.Again:
             raise HostTimeOut
@@ -108,7 +110,7 @@ class User:
             with self.lock:
                 self.peers_addr = [self.ipv6] 
             
-            for ip in convert_bin_to_ipv6_list(list_ips):
+            for ip in list_ips.split(', '):
                 self._connectPub(ip)
 
         dealer.close(1)
@@ -142,7 +144,7 @@ class User:
         for ip in self.peers_addr[1:]:
             self._disconnectPub(ip)
         
-        self.publisher.send_multipart([b'status', self.username.encode('utf-8'), (convert_ipv6_str_to_bin(self.ipv6)<<1 | 1)])
+        self.publisher.send_multipart([b'status', self.username.encode('utf-8'), (self.ipv6 + '0').encode('utf-8')])
 
         self.peers_addr.pop()
         self.peers -= 1
@@ -172,8 +174,8 @@ class User:
             topic, username, msg = self.subscriber.recv_multipart()
 
             if topic == b'status':
-                status = msg % 2
-                ip = convert_bin_to_ipv6_str(msg>>1)
+                status = bool(int(msg[-1]))
+                ip = msg[:-1].decode('utf-8')
 
                 if status:
                     self._connectPub(ip)
--- main2.py
+++ main2.py
@@ -3,7 +3,7 @@ from User import User
 
 user = User('dell')
 
-user.joinRoom('')
+user.joinRoom('3hCrJWK32xwQFXwhsvAdFcXgyxYwRncnu6eNdS8EFuuW', '123')
 
 print(user.invite)
 
